46438734075dd34e28dd5dbd26f5ff777b263cfa
Mike Mestebeld <mike@heumes.nl>
2026-04-11 13:47:35 +0200

    feat(plugin): Android project build.gradle Mapbox repo injection

---
+++ plugin/src/android/buildGradle.ts
@@ -0,0 +1,31 @@
+const MAPBOX_REPO_MARKER = '// expo-mapbox-navigation: authenticated Mapbox Maven repo';
+
+const MAPBOX_REPO_BLOCK = `    ${MAPBOX_REPO_MARKER}
+    maven {
+      url 'https://api.mapbox.com/downloads/v2/releases/maven'
+      authentication {
+        basic(BasicAuthentication)
+      }
+      credentials {
+        username = 'mapbox'
+        password = project.findProperty('MAPBOX_DOWNLOADS_TOKEN') ?: System.getenv('MAPBOX_DOWNLOADS_TOKEN') ?: ''
+      }
+    }`;
+
+export function applyMapboxProjectBuildGradle(contents: string): string {
+  if (contents.includes(MAPBOX_REPO_MARKER)) {
+    return contents;
+  }
+
+  const allProjectsRegex = /allprojects\s*\{\s*repositories\s*\{/;
+  if (!allProjectsRegex.test(contents)) {
+    throw new Error(
+      '[expo-mapbox-navigation] Could not find allprojects.repositories block in project build.gradle.'
+    );
+  }
+
+  return contents.replace(
+    allProjectsRegex,
+    (match) => `${match}\n${MAPBOX_REPO_BLOCK}\n`
+  );
+}
