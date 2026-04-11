d9fdfc08069a0a8159f195f8d31dd9cf8a2ad8e4
Mike Mestebeld <mike@heumes.nl>
2026-04-11 17:07:25 +0200

    refactor: reset JS module to setAccessToken skeleton

--- src/ExpoMapboxNavigationModule.ts
+++ src/ExpoMapboxNavigationModule.ts
@@ -1,11 +1,15 @@
 import { NativeModule, requireNativeModule } from 'expo';
 
-import { ExpoMapboxNavigationModuleEvents } from './ExpoMapboxNavigation.types';
+import type { MapboxNavigationModuleEvents } from './ExpoMapboxNavigation.types';
 
-declare class ExpoMapboxNavigationModule extends NativeModule<ExpoMapboxNavigationModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
+declare class ExpoMapboxNavigationModule extends NativeModule<MapboxNavigationModuleEvents> {
+  /**
+   * Set the Mapbox public access token at runtime. Safe to call multiple
+   * times; the latest value wins. Consumers can also supply the token
+   * at build time via the `expo-mapbox-navigation` config plugin, in
+   * which case this call is optional.
+   */
+  setAccessToken(token: string): void;
 }
 
 // This call loads the native module object from the JSI.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-// Reexport the native module. On web, it will be resolved to ExpoMapboxNavigationModule.web.ts
-// and on native platforms to ExpoMapboxNavigationModule.ts
 export { default } from './ExpoMapboxNavigationModule';
-export { default as ExpoMapboxNavigationView } from './ExpoMapboxNavigationView';
-export * from  './ExpoMapboxNavigation.types';
+export * from './ExpoMapboxNavigation.types';
